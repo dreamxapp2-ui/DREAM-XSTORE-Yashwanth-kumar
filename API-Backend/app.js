0fdb1f8b43eb2aa7e48745d4514dfe459a22520d
vikrant mahar <vikrantmahar@example.com>
2026-03-29 10:21:32 +0530

    fix: Fetch real product data on detail page and fix currency displa

--- API-Backend/app.js
+++ API-Backend/app.js
@@ -50,8 +50,8 @@ app.use(cors({
     },
     credentials: true
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(passport.initialize());
 
 // MongoDB Connection
